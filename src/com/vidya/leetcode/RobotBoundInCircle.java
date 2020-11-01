56e5858ddf63220e505fe2d49815b5521b69a034
Vidyasankar.Sundararaman <vidyasankar.sundararaman@VidyasankarSundararamans-MacBook-Pro.local>
2020-10-31 19:58:51 -0500

    files commited

---
+++ src/com/vidya/leetcode/RobotBoundInCircle.java
@@ -0,0 +1,105 @@
+package com.vidya.leetcode;
+
+/**
+ * On an infinite plane, a robot initially stands at (0, 0) and faces north.  The robot can receive one of three instructions:
+ *
+ * "G": go straight 1 unit;
+ * "L": turn 90 degrees to the left;
+ * "R": turn 90 degress to the right.
+ * The robot performs the instructions given in order, and repeats them forever.
+ *
+ * Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: "GGLLGG"
+ * Output: true
+ * Explanation:
+ * The robot moves from (0,0) to (0,2), turns 180 degrees, and then returns to (0,0).
+ * When repeating these instructions, the robot remains in the circle of radius 2 centered at the origin.
+ * Example 2:
+ *
+ * Input: "GG"
+ * Output: false
+ * Explanation:
+ * The robot moves north indefinitely.
+ * Example 3:
+ *
+ * Input: "GL"
+ * Output: true
+ * Explanation:
+ * The robot moves from (0, 0) -> (0, 1) -> (-1, 1) -> (-1, 0) -> (0, 0) -> ...
+ */
+public class RobotBoundInCircle {
+
+    public static void main(String[] args) {
+
+        String instructions  = "GGLLGG";
+        System.out.println(isRobotBoundInCircle(instructions));
+        instructions = "GG";
+        System.out.println(isRobotBoundInCircle(instructions));
+        instructions = "GL";
+        System.out.println(isRobotBoundInCircle(instructions));
+    }
+
+    static boolean isRobotBoundInCircle(String instructions){
+        if(instructions == null || instructions.length()==0){
+            return false;
+        }
+
+        int x = 0; // X coordinates
+        int y = 0; // Y coordinates
+        String directions = "North"; //Initial robot directions.
+
+        /*
+                    North
+             West           East
+                    South
+         */
+
+        for(char c: instructions.toCharArray()){
+            if(c == 'G'){
+                if(directions == "North"){
+                    y +=1;
+                }else if(directions == "South"){
+                    y -=1;
+                }else if(directions == "East"){
+                    x +=1;
+                }else {
+                    x -=1;
+                }
+            }else if(c == 'L'){
+                if(directions == "North"){
+                    directions = "West";
+                }else if(directions == "West"){
+                    directions = "South";
+                }else if(directions == "South"){
+                    directions = "East";
+                }else{
+                    directions = "North";
+                }
+            }else if(c == 'R'){
+                if(directions == "North"){
+                    directions = "East";
+                }else if(directions =="East"){
+                    directions = "South";
+                }else if(directions == "South"){
+                    directions = "West";
+                }else{
+                    directions = "North";
+                }
+            }
+        }
+
+        if(x ==0 &&  y ==0){
+            return true;
+        }
+        if(directions == "North"){
+            return false;
+        }
+        return true;
+    }
+
+}
