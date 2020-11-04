df2d83446d2fc799ba69fab1a16dc84ad12a36d9
Vidyasankar.Sundararaman <vidyasankar.sundararaman@VidyasankarSundararamans-MacBook-Pro.local>
2020-11-03 20:44:24 -0600

    new code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package com.vidya.leetcode;
-
 public class Main {
 
     public static void main(String[] args) {
--- src/com/vidya/leetcode/EditDistance.java
+++ src/com/vidya/leetcode/EditDistance.java
@@ -2,16 +2,16 @@ package com.vidya.leetcode;
 
 /**
  * Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
- *
+ * <p>
  * You have the following three operations permitted on a word:
- *
+ * <p>
  * Insert a character
  * Delete a character
  * Replace a character
- *
- *
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: word1 = "horse", word2 = "ros"
  * Output: 3
  * Explanation:
@@ -19,7 +19,7 @@ package com.vidya.leetcode;
  * rorse -> rose (remove 'r')
  * rose -> ros (remove 'e')
  * Example 2:
- *
+ * <p>
  * Input: word1 = "intention", word2 = "execution"
  * Output: 5
  * Explanation:
@@ -28,10 +28,10 @@ package com.vidya.leetcode;
  * enention -> exention (replace 'n' with 'x')
  * exention -> exection (replace 'n' with 'c')
  * exection -> execution (insert 'u')
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * 0 <= word1.length, word2.length <= 500
  * word1 and word2 consist of lowercase English letters.
  */
@@ -39,19 +39,30 @@ public class EditDistance {
 
     public static void main(String[] args) {
 
+        String word1 = "horse", word2 = "ros";
+
+        System.out.println(minDistance(word1,word2));
     }
 
-    static int minDistance(String word1, String word2){
+    static int minDistance(String word1, String word2) {
 
         // dp[i][j] : minimum steps to convert i long word1 and j long word2
-        int dp[][] = new int[word1.length()+1][word2.length()+1];
+        int dp[][] = new int[word1.length() + 1][word2.length() + 1];
 
+        for (int i = 0; i <= word1.length(); i++) dp[i][0] = i;
+        for (int j = 0; j <= word2.length(); j++) dp[0][j] = j;
 
-        for(int i=0;i<word1.length();i++) dp[i][0]= i;
-        for(int j=0;j<word2.length();j++) dp[0][j] = j;
-
-        
-
-
+        for (int i = 1;i <= word1.length(); i++) {
+            for (int j = 1; j<= word2.length(); j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1))// <--
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    // dp[i-1][j-1] : replace word1(i) with word2(j), because word1(0, i-1) == word2(0, j-1);
+                    // dp[i  ][j-1] : delete word(j)
+                    // dp[i-1][j  ] : delete word(i), because word1(0, i-1) == word2(0, j)
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j])) + 1;
+            }
+        }
+        return dp[word1.length()][word2.length()];
     }
 }
